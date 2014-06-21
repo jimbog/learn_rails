4c0d67a25970fb3624c012370770525118ca0161
bsusername <jim.my.garzon@gmail.com>
2014-06-21 17:36:09 -0400

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,8 @@ class VisitorsController < ApplicationController
 
 	def new
 		@owner = Owner.new
+    flash[:notice] = "welcome!"
+    flash[:alert] = "remember my birthday is coming up, get me a nice present"
 	end
 
 end
